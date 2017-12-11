bfbcaad4144f2c4c978ed91bbccce499544c6cf6
nanmanJack <786155625@qq.com>
2017-12-11 14:36:17 +0800

    test go func
    test go function and channels

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
+// test10 project main.go
 package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-  fmt.Println("Hello world")
+
+	c := make(chan int)
+	go func() {
+		for v := range c {
+			fmt.Println(v)
+		}
+	}()
+
+	for i := 0; i < 10; i++ {
+		select {
+		case c <- 1:
+		case c <- 2:
+		}
+	}
+
+	ch := make(chan bool)
+	select {
+	case v := <-ch:
+		fmt.Println(v)
+	case v := <-time.After(3 * time.Second):
+		fmt.Println("timeout", v)
+	}
+
 }
